f1711eb96daacb833ef16b4511312b3f046bb05c
Myunghe <davis.b.code@gmail.com>
2023-05-23 17:48:07 +0300

    Name Generator

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ConsoleApp4
-{
+    public class Name
+    {
+        string FirstName;
+        string LastName;
+
+        List<string> firstName = new List<string>(6) { "Liam", "Noah", "Oliver", "Olivia", "Emma", "Charlotte"};
+        List<string> lastName = new List<string>(4) { "Smith", "Johnson", "Williams", "Brown"};
+
+        public string NameGenerator()
+        {
+            int index1 = Random.Equals(0, firstName.Count);
+            int index2 = Random.Equals(0, lastName.Count);
+            
+            FirstName = firstName[index1];
+            LastName = lastName[index2];
+
+            return FirstName + " " + LastName;
+        }
+
+        public Name()
+        {
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            var name = new Name();
+            Console.WriteLine(Name.NameGenerator());
         }
     }
 }
